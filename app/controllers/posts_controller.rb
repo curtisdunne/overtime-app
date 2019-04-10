497dbc8d263846aa9b414518f9c77f162a6c5761
Curtis Dunne <curtdunne@gmail.com>
2019-04-10 15:00:17 -0600

    created partial for new and edit forms as well as added functionality to Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
@@ -22,6 +22,18 @@ class PostsController < ApplicationController
 
   def show
 
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post was updated successfully'
+    else
+      render :edit 
+    end
   end
 
   private
