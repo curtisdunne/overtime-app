6b7872f807d20963466bb644b80f9ff9029693f7
Curtis Dunne <curtdunne@gmail.com>
2019-04-15 16:07:58 -0600

    integrated permissions to hide status form element from non-admin users

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -9,14 +9,26 @@ describe 'navigate' do
   describe 'edit' do
     before do
       @post = FactoryGirl.create(:post)
+      visit edit_post_path(@post)
     end
 
-    it 'has a status that can be edited on the form' do
-      visit edit_post_path(@post)
-
+    it 'has a status that can be edited on the form by Admin' do
       choose('post_status_approved')
       click_on 'Save'
       expect(@post.reload.status).to eq('approved')
+    end
+
+    it 'can be edited by an Admin' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit edit_post_path(@post)
+      expect(page).to_not have_content('Approved')
+    end
+
+    xit 'cannot be edited by a non admin' do
+
     end
   end
 end
