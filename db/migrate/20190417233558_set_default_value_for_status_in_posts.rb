372ab8cd271d208ed3f2228997ebcd6443616738
Curtis Dunne <curtdunne@gmail.com>
2019-04-30 15:06:11 -0600

    updated one of the migration files that created the post field

--- db/migrate/20190417233558_set_default_value_for_status_in_posts.rb
+++ db/migrate/20190417233558_set_default_value_for_status_in_posts.rb
@@ -1,5 +1,5 @@
 class SetDefaultValueForStatusInPosts < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :status, :integer, default: 0
+    add_column :posts, :status, :integer, default: 0
   end
 end
